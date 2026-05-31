202b1fd50ea92d0b5f8995666509eebf566c3225
charley Yu <charley.yu@outlook.com>
2026-06-01 05:33:01 +0800

    Add configuration header for hardware settings

---
+++ config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Screen Hardware
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
+
+// Input Controls
+#define PIN_UP 2
+#define PIN_DOWN 3
+#define PIN_SELECT 4
+
+// Hardware Bus Triggers
+#define NFC_IRQ 5
+#define NFC_RST 6
+#define CC1101_GDO0 7
+
+#endif
